e1d3c1bc6aaf20eff37e1b3eabef10fbf8e1ae07
NgyAnthony <anthony.ng@hotmail.fr>
2017-02-24 16:16:55 +0100

    Update devinette.js

--- Activité 1/devinette.js
+++ Activité 1/devinette.js
@@ -10,27 +10,24 @@ console.log("(La solution est " + solution + ")");
 
 var saisie = prompt("Saisissez un nombre entre 1 et 100")
 
-while (solution === saisie){ // Si saisie === solution alors fin de boucle
+while (solution != saisie){ // Si saisie !== solution alors fin de boucle
     
     //Placement des limites entre 1 et 100 + reprise du jeu
     if ((saisie < 1) || (saisie > 100)){
-        console.log("Votre nombre n'est pas entre 1 et 100 !")
+        alert("Votre nombre n'est pas entre 1 et 100 !")
         var saisie = prompt("Saisissez un nombre entre 1 et 100")
     }
     
     //Indication que la saisie est supérieure à la solution + reprise
         else if(saisie > solution){
-            console.log(saisie + " est trop grand !")
+            alert(saisie + " est trop grand !")
             var saisie = prompt("Saisissez un nombre entre 1 et 100")
         }
     //Indication que la saisie est inférieure à la solution + reprise
             else if (saisie < solution){
-                console.log(saisie + " est trop petit !")
+                alert(saisie + " est trop petit !")
                 var saisie = prompt("Saisissez un nombre entre 1 et 100")
             }
-    //Si aucun des if confirmés alors solution === saisie 
-                else{
-                    console.log("Bravo! La solution était " + solution)
-} 
 
-}
+} 
+alert("Bravo! La solution était " + solution) 
