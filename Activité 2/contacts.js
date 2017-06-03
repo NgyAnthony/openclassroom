e62319717bfe3809b3ef4122f7772e275ad99eae
NgyAnthony <anthony.ng@hotmail.fr>
2017-06-03 21:48:01 +0200

    contacts (still) not working

--- Activité 2/contacts.js
+++ Activité 2/contacts.js
@@ -12,11 +12,20 @@ var contact = {
         this.prenom = prenom;
     },
     decrire : function (){
-        var description =  "Nom: " + this.nom + ", prenom : " + this.prenom
+        var description =  "Nom: " + this.nom + ", prenom : " + this.prenom;
         return description
+    },
+
+    ajout : function(){
+        console.log("Pour ajouter un contact entrez le nom et le prénom.");
+        var ajoutNom = prompt("Ajoutez le nom du contact");
+        var ajoutPrenom = prompt("Ajoutez le prénom de contact");
+        contact.init(ajoutNom, ajoutPrenom);
+        contacts.push(contact);
+        console.log("Le contact à bien été ajouté.")
     }
 
-}
+};
 
 var contact1 = Object.create(contact);
 contact1.init("Lévisse","Carole");
@@ -27,32 +36,26 @@ contact2.init("Nelsonne", "Mélodie");
 contacts.push(contact1);
 contacts.push(contact2);
 
+
 //Menu
 var choix = "";
-while (choix != 0){
-    choix = prompt("Choissisez une option");
+while (choix !== "0"){
     console.log("1 : Lister les contacts");
     console.log("2 : Ajouter un contact");
     console.log("0 : Quitter");
+    choix = prompt("Choissisez une option");
 
-    //Choix
-    if (choix = 1){
+    if (choix = "1"){
         contacts.forEach(function (contact){
             console.log(contact.decrire());
         })
-    }else if (choix = 2){
-        console.log("Pour ajouter un contact entrez le nom et le prénom.");
-        var ajoutNom = prompt("Ajoutez le nom du contact");
-        var ajoutPrenom = prompt("Ajoutez le prénom de contact");
-        contact.init(ajoutNom, ajoutPrenom)
-        contacts.push(contact)
+    }else if (choix = "2"){
+        contact.ajout();
+    }else {
+        console.log("Cette valeur m'est inconnue...")
     }
-
 }
 
-console.log("Au revoir !")
+console.log("Au revoir !");
 
-
-
-
-
+/* problème : while exécute le choix 1 tout le temps peu importe le choix...*/
