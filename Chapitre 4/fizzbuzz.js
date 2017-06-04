752eda48486f419dd2f222cef99730256ea81025
NgyAnthony <anthony.ng@hotmail.fr>
2017-06-04 17:19:33 +0200

    fizz buzz clean

--- Chapitre 4/fizzbuzz.js
+++ Chapitre 4/fizzbuzz.js
@@ -1,14 +1,11 @@
-var nombre = prompt("Entrez un nombre entre 1 et 100")
-while(nombre <= 100){
-    nombre++
-    console.log(nombre)
-    if (nombre % 3 === 0){
-        console.log ("Fizz")
+for (var nombre = 1; nombre <=100; nombre++) {
+    if ((nombre % 5 === 0) & (nombre % 3 === 0)) {
+        console.log("FizzBuzz")
+    } else if (nombre % 3 === 0) {
+        console.log("Fizz")
+    } else if (nombre % 5 === 0) {
+        console.log("Buzz")
+    } else {
+        console.log(nombre)
     }
-        else if (nombre % 5 === 0){
-            console.log("Buzz")
-        }
-        if((nombre % 5 === 0) & (nombre % 3 === 0) ){
-                console.log("FizzBuzz")
-            }
-} 
+}
